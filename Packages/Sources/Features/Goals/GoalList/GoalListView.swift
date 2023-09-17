bd3afa905b2f154ef703aecaf3424add0f6e64f2
Sam Watts <samuel.watts@gmail.com>
2023-09-17 13:23:39 +0100

    added extra view to help with preview

--- Packages/Sources/Features/Goals/GoalList/GoalListView.swift
+++ Packages/Sources/Features/Goals/GoalList/GoalListView.swift
@@ -105,6 +105,76 @@ public struct GoalListView: View {
     }
 }
 
+struct UpdatedGoalPreviewWrapperFeature: Reducer {
+    struct State: Equatable {
+        var goalList: GoalListFeature.State
+    }
+    
+    enum Action: Equatable {
+        enum View: Equatable {
+            case onAppear
+        }
+        
+        enum Internal: Equatable {
+            case refreshGoals
+        }
+        
+        case view(View)
+        case _internal(Internal)
+        case goalList(GoalListFeature.Action)
+    }
+    
+    @Dependency(\.mainQueue) var mainQueue
+    @Dependency(\.repository.goals) var goals
+    @Dependency(\.repository.runningWorkouts) var runningWorkouts
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case let .view(action):
+                return view(action, state: &state)
+            case let ._internal(action):
+                return _internal(action, state: &state)
+            case .goalList:
+                return .none
+            }
+        }
+        Scope(state: \.goalList, action: /Action.goalList, child: GoalListFeature.init)
+    }
+    
+    private func view(_ action: Action.View, state: inout State) -> EffectOf<Self> {
+        switch action {
+        case .onAppear:
+            return .run { send in
+                try await mainQueue.sleep(for: .seconds(2))
+                await send(._internal(.refreshGoals))
+            }
+        }
+    }
+    
+    private func _internal(_ action: Action.Internal, state: inout State) -> EffectOf<Self> {
+        switch action {
+        case .refreshGoals:
+            state.goalList.refresh(goals: goals, runningWorkouts: runningWorkouts)
+            return .none
+        }
+    }
+}
+
+struct UpdatedGoalPreviewWrapper: View {
+    let store: StoreOf<UpdatedGoalPreviewWrapperFeature>
+    
+    var body: some View {
+        GoalListView(
+            store: store.scope(
+                state: \.goalList,
+                action: UpdatedGoalPreviewWrapperFeature.Action.goalList
+            )
+        )
+        .onAppear { store.send(.view(.onAppear)) }
+    }
+}
+
 struct GoalListView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationStack {
@@ -124,10 +194,10 @@ struct GoalListView_Previews: PreviewProvider {
         let test: LockIsolated<Int> = .init(0)
 
         NavigationStack {
-            GoalListView(
+            UpdatedGoalPreviewWrapper(
                 store: .init(
-                    initialState: .init(),
-                    reducer: GoalListFeature.init,
+                    initialState: .init(goalList: .init()),
+                    reducer: UpdatedGoalPreviewWrapperFeature.init,
                     withDependencies: {
                         $0.date = .constant(.preview)
                         $0.locale = .init(identifier: "en_AU")
