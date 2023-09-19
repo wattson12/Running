1824abc9757fc13f9341fb4b99ad0f581e95a73c
Sam Watts <samuel.watts@gmail.com>
2023-09-19 21:07:25 +0100

    Added reducer

---
+++ Packages/Sources/Dependencies/Logging/Helpers.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+func typeName(_ type: Any.Type) -> String {
+    var name = _typeName(type, qualified: true)
+    if let index = name.firstIndex(of: ".") {
+        name.removeSubrange(...index)
+    }
+    let sanitizedName =
+        name
+            .replacingOccurrences(
+                of: #"<.+>|\(unknown context at \$[[:xdigit:]]+\)\."#,
+                with: "",
+                options: .regularExpression
+            )
+    return sanitizedName
+}
+
+func debugCaseOutput(
+    _ value: Any,
+    abbreviated: Bool = false
+) -> String {
+    func debugCaseOutputHelp(_ value: Any) -> String {
+        let mirror = Mirror(reflecting: value)
+        switch mirror.displayStyle {
+        case .enum:
+            guard let child = mirror.children.first else {
+                let childOutput = "\(value)"
+                return childOutput == "\(typeName(type(of: value)))" ? "" : ".\(childOutput)"
+            }
+            let childOutput = debugCaseOutputHelp(child.value)
+            return ".\(child.label ?? "")\(childOutput.isEmpty ? "" : "(\(childOutput))")"
+        case .tuple:
+            return mirror.children.map { label, value in
+                let childOutput = debugCaseOutputHelp(value)
+                return "\(label.map { isUnlabeledArgument($0) ? "_:" : "\($0):" } ?? "")\(childOutput.isEmpty ? "" : " \(childOutput)")"
+            }
+            .joined(separator: ", ")
+        default:
+            return ""
+        }
+    }
+
+    return (value as? CustomDebugStringConvertible)?.debugDescription
+        ?? "\(abbreviated ? "" : typeName(type(of: value)))\(debugCaseOutputHelp(value))"
+}
+
+private func isUnlabeledArgument(_ label: String) -> Bool {
+    label.firstIndex(where: { $0 != "." && !$0.isNumber }) == nil
+}
--- Packages/Sources/Dependencies/Logging/LoggingReducer.swift
+++ Packages/Sources/Dependencies/Logging/LoggingReducer.swift
@@ -1,2 +1,59 @@
 import ComposableArchitecture
 import Foundation
+
+public struct _LoggingReducer<R: Reducer>: Reducer {
+    let base: R
+
+    public init(base: R) {
+        self.base = base
+    }
+
+    @Dependency(\.logStore) var logStore
+    @Dependency(\.date) var date
+    @Dependency(\.uuid) var uuid
+
+    public func reduce(into state: inout R.State, action: R.Action) -> Effect<R.Action> {
+        let originalState = state
+        let baseEffect = base.reduce(into: &state, action: action)
+
+        return baseEffect
+            .merge(
+                with: .run { [newState = state] _ in
+                    logStore.append(
+                        log: log(
+                            originalState: originalState,
+                            newState: newState,
+                            action: action
+                        )
+                    )
+                }
+            )
+    }
+
+    private func log(originalState: R.State, newState: R.State, action: R.Action) -> ActionLog {
+        var actionDump = ""
+        CustomDump.customDump(action, to: &actionDump, indent: 4)
+
+        var stateDiffDump = ""
+        if let difference = diff(originalState, newState) {
+            stateDiffDump = difference
+        }
+
+        let label = debugCaseOutput(action)
+        let diffLines = stateDiffDump.components(separatedBy: .newlines)
+
+        return ActionLog(
+            id: uuid(),
+            timestamp: date.now,
+            actionLabel: label,
+            action: actionDump,
+            stateDiff: diffLines.isEmpty ? nil : diffLines
+        )
+    }
+}
+
+public extension Reducer {
+    func _logging() -> _LoggingReducer<Self> {
+        _LoggingReducer(base: self)
+    }
+}
