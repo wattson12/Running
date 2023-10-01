20cef455771505a0e4e77d8d1a5cb7d50286d31d
Sam Watts <samuel.watts@gmail.com>
2023-10-01 21:53:27 +0100

    fetch runs

--- RunningDebug/DebugRunListView.swift
+++ RunningDebug/DebugRunListView.swift
@@ -13,14 +13,23 @@ struct DebugRunListFeature: Reducer {
             case onAppear
         }
 
+        enum Internal: Equatable {
+            case runsFetched(TaskResult<[Run]>)
+        }
+
         case view(View)
+        case _internal(Internal)
     }
+
+    @Dependency(\.repository.runningWorkouts) var runningWorkouts
 
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
             case let .view(action):
                 return view(action, state: &state)
+            case let ._internal(action):
+                return _internal(action, state: &state)
             }
         }
     }
@@ -28,6 +37,22 @@ struct DebugRunListFeature: Reducer {
     private func view(_ action: Action.View, state _: inout State) -> EffectOf<Self> {
         switch action {
         case .onAppear:
+            return .run { send in
+                let result = await TaskResult {
+                    try await runningWorkouts.allRunningWorkouts.remote()
+                }
+                await send(._internal(.runsFetched(result)))
+            }
+        }
+    }
+
+    private func _internal(_ action: Action.Internal, state: inout State) -> EffectOf<Self> {
+        switch action {
+        case let .runsFetched(.success(runs)):
+            state.runs = .init(uniqueElements: runs)
+            return .none
+        case let .runsFetched(.failure(error)):
+            print(error)
             return .none
         }
     }
@@ -37,15 +62,23 @@ struct DebugRunListView: View {
     let store: StoreOf<DebugRunListFeature>
 
     var body: some View {
-        Text("Placeholder Run List View")
+        WithViewStore(store, observe: { $0 }) { viewStore in
+            List(viewStore.runs) { run in
+                Text(run.distance.formatted())
+            }
+            .onAppear { viewStore.send(.view(.onAppear)) }
+        }
     }
 }
 
 #Preview {
-    DebugRunListView(
-        store: .init(
-            initialState: .init(),
-            reducer: DebugRunListFeature.init
+    NavigationStack {
+        DebugRunListView(
+            store: .init(
+                initialState: .init(),
+                reducer: DebugRunListFeature.init
+            )
         )
-    )
+        .navigationTitle("Debug")
+    }
 }
