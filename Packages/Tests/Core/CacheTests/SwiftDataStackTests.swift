5659ecac37fce6c06fbd8001a28ce7751ff8056c
Sam Watts <samuel.watts@gmail.com>
2023-10-23 21:42:47 +0100

    removed old tests

--- Packages/Tests/Core/CacheTests/SwiftDataStackTests.swift
+++ Packages/Tests/Core/CacheTests/SwiftDataStackTests.swift
@@ -2,28 +2,4 @@
 import SwiftData
 import XCTest
 
-final class SwiftDataStackTests: XCTestCase {
-    func testStackIsCreatedCorrectly() throws {
-        let sut = SwiftDataStack.stack(inMemory: false)
-        let context = try sut.context()
-
-        let entities = context.container.schema.entitiesByName
-        XCTAssertEqual(entities.keys.sorted(), ["DistanceSample", "Goal", "Location", "Run", "RunDetail"])
-
-        let configuration = try XCTUnwrap(context.container.configurations.first)
-        XCTAssertEqual(configuration.isStoredInMemoryOnly, false)
-        XCTAssertNil(configuration.cloudKitContainerIdentifier)
-    }
-
-    func testStackIsCreatedCorrectlyForInMemory() throws {
-        let sut = SwiftDataStack.stack(inMemory: true)
-        let context = try sut.context()
-
-        let entities = context.container.schema.entitiesByName
-        XCTAssertEqual(entities.keys.sorted(), ["DistanceSample", "Goal", "Location", "Run", "RunDetail"])
-
-        let configuration = try XCTUnwrap(context.container.configurations.first)
-        XCTAssertEqual(configuration.isStoredInMemoryOnly, true)
-        XCTAssertNil(configuration.cloudKitContainerIdentifier)
-    }
-}
+final class SwiftDataStackTests: XCTestCase {}
