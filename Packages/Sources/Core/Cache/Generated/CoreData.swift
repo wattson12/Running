02f75e8c53c0692dc80d5b80a39663a08281913f
Sam Watts <samuel.watts@gmail.com>
2023-10-23 20:17:32 +0100

    generated core data models

---
+++ Packages/Sources/Core/Cache/Generated/CoreData.swift
@@ -0,0 +1,32 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+// swiftlint:disable superfluous_disable_command implicit_return
+// swiftlint:disable sorted_imports
+import CoreData
+import Foundation
+
+// swiftlint:disable attributes file_length vertical_whitespace_closing_braces
+// swiftlint:disable identifier_name line_length type_body_length
+
+// MARK: - DistanceSampleEntity
+
+// Note: 'DistanceSampleEntity' has codegen enabled for Xcode, skipping code generation.
+
+// MARK: - GoalEntity
+
+// Note: 'GoalEntity' has codegen enabled for Xcode, skipping code generation.
+
+// MARK: - LocationEntity
+
+// Note: 'LocationEntity' has codegen enabled for Xcode, skipping code generation.
+
+// MARK: - RunDetailEntity
+
+// Note: 'RunDetailEntity' has codegen enabled for Xcode, skipping code generation.
+
+// MARK: - RunEntity
+
+// Note: 'RunEntity' has codegen enabled for Xcode, skipping code generation.
+
+// swiftlint:enable identifier_name line_length type_body_length
