56a6fb77947801d787afd7ceb4c77bedd694b6e5
Sam Watts <samuel.watts@gmail.com>
2023-10-23 20:18:33 +0100

    switched to manual generation

--- Packages/Sources/Core/Cache/Generated/CoreData.swift
+++ Packages/Sources/Core/Cache/Generated/CoreData.swift
@@ -11,22 +11,122 @@ import Foundation
 
 // MARK: - DistanceSampleEntity
 
-// Note: 'DistanceSampleEntity' has codegen enabled for Xcode, skipping code generation.
+public final class DistanceSampleEntity: NSManagedObject {
+    public class var entityName: String {
+        "DistanceSampleEntity"
+    }
+
+    public class func entity(in managedObjectContext: NSManagedObjectContext) -> NSEntityDescription? {
+        NSEntityDescription.entity(forEntityName: entityName, in: managedObjectContext)
+    }
+
+    @available(*, deprecated, renamed: "makeFetchRequest", message: "To avoid collisions with the less concrete method in `NSManagedObject`, please use `makeFetchRequest()` instead.")
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<DistanceSampleEntity> {
+        NSFetchRequest<DistanceSampleEntity>(entityName: entityName)
+    }
+
+    @nonobjc public class func makeFetchRequest() -> NSFetchRequest<DistanceSampleEntity> {
+        NSFetchRequest<DistanceSampleEntity>(entityName: entityName)
+    }
+
+    // swiftlint:disable discouraged_optional_boolean discouraged_optional_collection implicit_getter
+    // swiftlint:enable discouraged_optional_boolean discouraged_optional_collection implicit_getter
+}
 
 // MARK: - GoalEntity
 
-// Note: 'GoalEntity' has codegen enabled for Xcode, skipping code generation.
+public final class GoalEntity: NSManagedObject {
+    public class var entityName: String {
+        "GoalEntity"
+    }
+
+    public class func entity(in managedObjectContext: NSManagedObjectContext) -> NSEntityDescription? {
+        NSEntityDescription.entity(forEntityName: entityName, in: managedObjectContext)
+    }
+
+    @available(*, deprecated, renamed: "makeFetchRequest", message: "To avoid collisions with the less concrete method in `NSManagedObject`, please use `makeFetchRequest()` instead.")
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<GoalEntity> {
+        NSFetchRequest<GoalEntity>(entityName: entityName)
+    }
+
+    @nonobjc public class func makeFetchRequest() -> NSFetchRequest<GoalEntity> {
+        NSFetchRequest<GoalEntity>(entityName: entityName)
+    }
+
+    // swiftlint:disable discouraged_optional_boolean discouraged_optional_collection implicit_getter
+    // swiftlint:enable discouraged_optional_boolean discouraged_optional_collection implicit_getter
+}
 
 // MARK: - LocationEntity
 
-// Note: 'LocationEntity' has codegen enabled for Xcode, skipping code generation.
+public final class LocationEntity: NSManagedObject {
+    public class var entityName: String {
+        "LocationEntity"
+    }
+
+    public class func entity(in managedObjectContext: NSManagedObjectContext) -> NSEntityDescription? {
+        NSEntityDescription.entity(forEntityName: entityName, in: managedObjectContext)
+    }
+
+    @available(*, deprecated, renamed: "makeFetchRequest", message: "To avoid collisions with the less concrete method in `NSManagedObject`, please use `makeFetchRequest()` instead.")
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<LocationEntity> {
+        NSFetchRequest<LocationEntity>(entityName: entityName)
+    }
+
+    @nonobjc public class func makeFetchRequest() -> NSFetchRequest<LocationEntity> {
+        NSFetchRequest<LocationEntity>(entityName: entityName)
+    }
+
+    // swiftlint:disable discouraged_optional_boolean discouraged_optional_collection implicit_getter
+    // swiftlint:enable discouraged_optional_boolean discouraged_optional_collection implicit_getter
+}
 
 // MARK: - RunDetailEntity
 
-// Note: 'RunDetailEntity' has codegen enabled for Xcode, skipping code generation.
+public final class RunDetailEntity: NSManagedObject {
+    public class var entityName: String {
+        "RunDetailEntity"
+    }
+
+    public class func entity(in managedObjectContext: NSManagedObjectContext) -> NSEntityDescription? {
+        NSEntityDescription.entity(forEntityName: entityName, in: managedObjectContext)
+    }
+
+    @available(*, deprecated, renamed: "makeFetchRequest", message: "To avoid collisions with the less concrete method in `NSManagedObject`, please use `makeFetchRequest()` instead.")
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<RunDetailEntity> {
+        NSFetchRequest<RunDetailEntity>(entityName: entityName)
+    }
+
+    @nonobjc public class func makeFetchRequest() -> NSFetchRequest<RunDetailEntity> {
+        NSFetchRequest<RunDetailEntity>(entityName: entityName)
+    }
+
+    // swiftlint:disable discouraged_optional_boolean discouraged_optional_collection implicit_getter
+    // swiftlint:enable discouraged_optional_boolean discouraged_optional_collection implicit_getter
+}
 
 // MARK: - RunEntity
 
-// Note: 'RunEntity' has codegen enabled for Xcode, skipping code generation.
+public final class RunEntity: NSManagedObject {
+    public class var entityName: String {
+        "RunEntity"
+    }
+
+    public class func entity(in managedObjectContext: NSManagedObjectContext) -> NSEntityDescription? {
+        NSEntityDescription.entity(forEntityName: entityName, in: managedObjectContext)
+    }
+
+    @available(*, deprecated, renamed: "makeFetchRequest", message: "To avoid collisions with the less concrete method in `NSManagedObject`, please use `makeFetchRequest()` instead.")
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<RunEntity> {
+        NSFetchRequest<RunEntity>(entityName: entityName)
+    }
+
+    @nonobjc public class func makeFetchRequest() -> NSFetchRequest<RunEntity> {
+        NSFetchRequest<RunEntity>(entityName: entityName)
+    }
+
+    // swiftlint:disable discouraged_optional_boolean discouraged_optional_collection implicit_getter
+    // swiftlint:enable discouraged_optional_boolean discouraged_optional_collection implicit_getter
+}
 
 // swiftlint:enable identifier_name line_length type_body_length
