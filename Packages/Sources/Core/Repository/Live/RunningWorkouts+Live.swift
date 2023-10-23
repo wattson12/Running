8d2c02ffea53053501f52f1c2943650ba79c1c2a
Sam Watts <samuel.watts@gmail.com>
2023-10-23 21:21:26 +0100

    [WIP] updating running workouts and tests

--- Packages/Sources/Core/Repository/Live/RunningWorkouts+Live.swift
+++ Packages/Sources/Core/Repository/Live/RunningWorkouts+Live.swift
@@ -3,11 +3,11 @@ import Dependencies
 import Foundation
 import HealthKitServiceInterface
 import Model
-import SwiftData
 
 extension RunningWorkouts {
     static func live() -> Self {
         @Dependency(\.swiftData) var swiftData
+        @Dependency(\.coreData) var coreData
         @Dependency(\.healthKit.runningWorkouts) var healthKitRunningWorkouts
         @Dependency(\.calendar) var calendar
         @Dependency(\.date) var date
@@ -16,12 +16,12 @@ extension RunningWorkouts {
             allRunningWorkouts: .init(
                 cache: {
                     Implementation.cachedRunningWorkouts(
-                        swiftData: swiftData
+                        coreData: coreData
                     )
                 },
                 remote: {
                     try await Implementation.remoteRunningWorkouts(
-                        swiftData: swiftData,
+                        coreData: coreData,
                         healthKitRunningWorkouts: healthKitRunningWorkouts
                     )
                 }
@@ -46,15 +46,17 @@ extension RunningWorkouts {
 
     private enum Implementation {
         static func cachedRunningWorkouts(
-            swiftData: SwiftDataStack
+            coreData: CoreDataStack
         ) -> [Model.Run]? {
             do {
-                let context = try swiftData.context()
-                let descriptor: FetchDescriptor<Cache.Run> = FetchDescriptor(sortBy: [.init(\.startDate, order: .forward)])
-
-                let runs = try context.fetch(descriptor)
-                    .map(Run.init(cached:))
-                return runs.isEmpty ? nil : runs
+                return try coreData.performWork { context in
+                    let fetchRequest = Cache.RunEntity.makeFetchRequest()
+                    fetchRequest.sortDescriptors = [
+                        .init(keyPath: \Cache.RunEntity.startDate, ascending: true),
+                    ]
+                    let runs = try context.fetch(fetchRequest)
+                    return runs.isEmpty ? nil : runs.map(Model.Run.init(entity:))
+                }
             } catch {
                 return nil
             }
@@ -62,56 +64,47 @@ extension RunningWorkouts {
 
         @MainActor
         static func remoteRunningWorkouts(
-            swiftData: SwiftDataStack,
+            coreData: CoreDataStack,
             healthKitRunningWorkouts: HealthKitRunningWorkouts
         ) async throws -> [Model.Run] {
             let runs = try await healthKitRunningWorkouts
                 .allRunningWorkouts()
                 .compactMap(Run.init(model:))
 
-            let context = try swiftData.context()
+            return try coreData.performWork { context in
 
-            var runsNeedingUpdate: [Model.Run.ID: Cache.Run] = [:]
-            for run in runs {
-                let runsMatchingID = try context.fetch(
-                    .init(
-                        predicate: #Predicate<Cache.Run> { [id = run.id] run in
-                            run.id == id
-                        }
-                    )
-                )
-                if let existingRun = runsMatchingID.first {
-                    existingRun.startDate = run.startDate
-                    existingRun.distance = run.distance.value
-                    existingRun.duration = run.duration.value
-                    runsNeedingUpdate[existingRun.id] = existingRun
-                } else {
-                    let cacheValue = Cache.Run(
-                        id: run.id,
-                        startDate: run.startDate,
-                        distance: run.distance.value,
-                        duration: run.duration.value,
-                        detail: nil
-                    )
-                    context.insert(cacheValue)
-                }
-            }
+                var runsNeedingUpdate: [Model.Run.ID: Cache.RunEntity] = [:]
+                for run in runs {
+                    let fetchRequestForRunsMatchingID = Cache.RunEntity.makeFetchRequest()
+                    fetchRequestForRunsMatchingID.predicate = .init(format: "id == %@", run.id.uuidString)
+                    let runsMatchingID = try context.fetch(fetchRequestForRunsMatchingID)
 
-            let responseIDs = runs.map(\.id)
-            let runsNotInResponse = try context.fetch(
-                .init(
-                    predicate: #Predicate<Cache.Run> { run in
-                        !responseIDs.contains(run.id)
+                    if let existingRun = runsMatchingID.first {
+                        existingRun.startDate = run.startDate
+                        existingRun.distance = run.distance.value
+                        existingRun.duration = run.duration.value
+                        runsNeedingUpdate[existingRun.id] = existingRun
+                    } else {
+                        let newRun = Cache.RunEntity(context: context)
+                        newRun.id = run.id
+                        newRun.startDate = run.startDate
+                        newRun.distance = run.distance.converted(to: .meters).value
+                        newRun.duration = run.duration.converted(to: .seconds).value
                     }
-                )
-            )
-            runsNotInResponse.forEach(context.delete)
+                }
 
-            try context.save()
+                let responseIDs = runs.map(\.id)
+                let fetchRequestForRunsNotInResponse = Cache.RunEntity.makeFetchRequest()
+                fetchRequestForRunsNotInResponse.predicate = .init(format: "NOT (id IN %@)", responseIDs)
+                let runsNotInResponse = try context.fetch(fetchRequestForRunsNotInResponse)
+                runsNotInResponse.forEach(context.delete)
 
-            return runs.map { run in
-                guard let updatedRunEntity = runsNeedingUpdate[run.id] else { return run }
-                return .init(cached: updatedRunEntity)
+                try context.save()
+
+                return runs.map { run in
+                    guard let updatedRunEntity = runsNeedingUpdate[run.id] else { return run }
+                    return .init(entity: updatedRunEntity)
+                }
             }
         }
 
@@ -164,26 +157,27 @@ extension RunningWorkouts {
         #warning("remove this and instead add an optional predicate / date range to the allRunningWorkouts function")
         // this could be an extension with the same signature which uses the date range
         static func runsWithinGoal(
-            goal: Model.Goal,
-            swiftData: SwiftDataStack,
-            calendar: Calendar,
-            date: Date
+            goal _: Model.Goal,
+            swiftData _: SwiftDataStack,
+            calendar _: Calendar,
+            date _: Date
         ) throws -> [Model.Run] {
-            guard let dates = goal.period.startAndEnd(in: calendar, now: date) else {
-                throw RunningWorkoutsError.validation("Unable to create date range from goal: \(goal)")
-            }
-            let start = dates.start
-            let end = dates.end
-
-            let context = try swiftData.context()
-            let descriptor: FetchDescriptor<Cache.Run> = FetchDescriptor(
-                predicate: #Predicate<Cache.Run> {
-                    $0.startDate >= start && $0.startDate < end
-                },
-                sortBy: [.init(\.startDate, order: .forward)]
-            )
-
-            return try context.fetch(descriptor).map(Run.init(cached:))
+            []
+//            guard let dates = goal.period.startAndEnd(in: calendar, now: date) else {
+//                throw RunningWorkoutsError.validation("Unable to create date range from goal: \(goal)")
+//            }
+//            let start = dates.start
+//            let end = dates.end
+//
+//            let context = try swiftData.context()
+//            let descriptor: FetchDescriptor<Cache.Run> = FetchDescriptor(
+//                predicate: #Predicate<Cache.Run> {
+//                    $0.startDate >= start && $0.startDate < end
+//                },
+//                sortBy: [.init(\.startDate, order: .forward)]
+//            )
+//
+//            return try context.fetch(descriptor).map(Run.init(cached:))
         }
     }
 }
