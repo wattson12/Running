00196932f19217c4fdbd4efc24d5ac867b01c3ee
Sam Watts <samuel.watts@gmail.com>
2024-01-05 21:08:03 +0000

    added empty state to history view

--- Packages/Sources/Features/History/HistoryView.swift
+++ Packages/Sources/Features/History/HistoryView.swift
@@ -15,71 +15,75 @@ public struct HistoryView: View {
     }
 
     public var body: some View {
-        List {
-            Section(
-                content: {
-                    ForEach(store.totals) { total in
-                        HStack {
-                            Text(total.label)
-                            Spacer()
-                            Text(total.distance.fullValue(locale: locale))
+        if store.totals.isEmpty {
+            HistoryEmptyView()
+        } else {
+            List {
+                Section(
+                    content: {
+                        ForEach(store.totals) { total in
+                            HStack {
+                                Text(total.label)
+                                Spacer()
+                                Text(total.distance.fullValue(locale: locale))
+                            }
+                        }
+                    },
+                    footer: {
+                        if let summary = store.summary {
+                            VStack(alignment: .leading) {
+                                Text("Distance: \(summary.distance.fullValue(locale: locale))")
+                                Text("Duration: \(summary.duration.summaryValue(locale: locale))")
+                                Text("Count: \(summary.count.description)")
+                            }
                         }
                     }
-                },
-                footer: {
-                    if let summary = store.summary {
-                        VStack(alignment: .leading) {
-                            Text("Distance: \(summary.distance.fullValue(locale: locale))")
-                            Text("Duration: \(summary.duration.summaryValue(locale: locale))")
-                            Text("Count: \(summary.count.description)")
+                )
+            }
+            .animation(.default, value: store.sortType)
+            .onAppear { store.send(.view(.onAppear)) }
+            .navigationTitle(L10n.History.title)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Menu {
+                        Section(L10n.History.Menu.Sort.title) {
+                            Button(
+                                action: {
+                                    store.send(.view(.sortByDateMenuButtonTapped))
+                                },
+                                label: {
+                                    HStack {
+                                        if store.sortType == .date {
+                                            Image(systemName: "checkmark")
+                                        }
+                                        Text(L10n.History.Menu.Sort.date)
+                                    }
+                                }
+                            )
+                            Button(
+                                action: {
+                                    store.send(.view(.sortByDistanceMenuButtonTapped))
+                                },
+                                label: {
+                                    HStack {
+                                        if store.sortType == .distance {
+                                            Image(systemName: "checkmark")
+                                        }
+                                        Text(L10n.History.Menu.Sort.distance)
+                                    }
+                                }
+                            )
                         }
+                    } label: {
+                        Label(L10n.History.Menu.label, systemImage: "arrow.up.arrow.down")
                     }
-                }
-            )
-        }
-        .animation(.default, value: store.sortType)
-        .onAppear { store.send(.view(.onAppear)) }
-        .navigationTitle(L10n.History.title)
-        .toolbar {
-            ToolbarItem(placement: .topBarTrailing) {
-                Menu {
-                    Section(L10n.History.Menu.Sort.title) {
-                        Button(
-                            action: {
-                                store.send(.view(.sortByDateMenuButtonTapped))
-                            },
-                            label: {
-                                HStack {
-                                    if store.sortType == .date {
-                                        Image(systemName: "checkmark")
-                                    }
-                                    Text(L10n.History.Menu.Sort.date)
-                                }
-                            }
-                        )
-                        Button(
-                            action: {
-                                store.send(.view(.sortByDistanceMenuButtonTapped))
-                            },
-                            label: {
-                                HStack {
-                                    if store.sortType == .distance {
-                                        Image(systemName: "checkmark")
-                                    }
-                                    Text(L10n.History.Menu.Sort.distance)
-                                }
-                            }
-                        )
-                    }
-                } label: {
-                    Label(L10n.History.Menu.label, systemImage: "arrow.up.arrow.down")
                 }
             }
         }
     }
 }
 
-#Preview {
+#Preview("Populated") {
     NavigationStack {
         HistoryView(
             store: .init(
@@ -97,6 +101,25 @@ public struct HistoryView: View {
                     ]
                 ),
                 reducer: { HistoryFeature() }
+            )
+        )
+        .environment(\.locale, .init(identifier: "en-AU"))
+    }
+}
+
+#Preview("Empty") {
+    NavigationStack {
+        HistoryView(
+            store: .init(
+                initialState: HistoryFeature.State(
+                    totals: []
+                ),
+                reducer: { HistoryFeature() },
+                withDependencies: {
+                    $0.repository.runningWorkouts._allRunningWorkouts = {
+                        .mock(value: [])
+                    }
+                }
             )
         )
         .environment(\.locale, .init(identifier: "en-AU"))
