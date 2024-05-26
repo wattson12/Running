7e354336276250962161784f9569152a39dc58b6
Sam Watts <samuel.watts@gmail.com>
2024-05-26 21:21:02 +0100

    updated info.plist path

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,6 +5,6 @@ echo "Skipping macro validation"
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
 
 echo "Setting environment variable for IS_TESTFLIGHT_BUILD"
-plutil -replace IS_TESTFLIGHT_BUILD -string $IS_TESTFLIGHT_BUILD Running/Info.plist
+plutil -replace IS_TESTFLIGHT_BUILD -string $IS_TESTFLIGHT_BUILD ../Running/Info.plist
 
-plutil -p Running/Info.plist
+plutil -p ../Running/Info.plist
