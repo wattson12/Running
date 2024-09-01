53c50020c387e0929b47d34156ba47a712b6cf13
Sam Watts <samuel.watts@gmail.com>
2024-09-01 20:59:00 +0100

    first pass at history implementation

--- Packages/Sources/Features/Goals/GoalHistory/GoalHistoryView.swift
+++ Packages/Sources/Features/Goals/GoalHistory/GoalHistoryView.swift
@@ -1,23 +1,85 @@
 import ComposableArchitecture
+import Dependencies
+import Model
+import Repository
 import SwiftUI
 
 @Reducer
 public struct GoalHistoryFeature {
     public struct State: Equatable {
-        public init() {}
+        let period: Goal.Period
+
+        public init(period: Goal.Period) {
+            self.period = period
+        }
     }
 
-    public enum Action: Equatable {}
+    public enum Action: ViewAction, Equatable {
+        public enum View: Equatable {
+            case onAppear
+        }
+
+        case view(View)
+    }
+
+    @Dependency(\.repository.runningWorkouts) var runningWorkouts
+    @Dependency(\.repository.goals) var goals
+    @Dependency(\.calendar) var calendar
+    @Dependency(\.date) var date
 
     public init() {}
 
     public var body: some ReducerOf<Self> {
-        EmptyReducer()
+        Reduce { state, action in
+            switch action {
+            case let .view(action):
+                return view(action, state: &state)
+            }
+        }
+    }
+
+    private func view(_ action: Action.View, state: inout State) -> EffectOf<Self> {
+        switch action {
+        case .onAppear:
+            guard let cachedRuns = runningWorkouts.allRunningWorkouts.cache(), let firstRun = cachedRuns.first else { return .none }
+
+            var ranges: [DateRange] = []
+            guard var range = state.period.startAndEnd(in: calendar, now: date.now) else { return .none }
+            ranges.append(.init(period: state.period, start: range.start, end: range.end))
+
+            while range.end > firstRun.startDate {
+                let offsetComponent: Calendar.Component
+                switch state.period {
+                case .weekly:
+                    offsetComponent = .weekOfYear
+                case .monthly:
+                    offsetComponent = .month
+                case .yearly:
+                    offsetComponent = .year
+                }
+
+                guard let newStart = calendar.date(byAdding: offsetComponent, value: -1, to: range.start) else {
+                    break
+                }
+                guard newStart <= date.now else { break }
+                guard let newEnd = calendar.date(byAdding: offsetComponent, value: -1, to: range.end) else { break }
+
+                let newRange: DateRange = .init(period: state.period, start: newStart, end: newEnd)
+                ranges.append(newRange)
+                range = (newRange.start, newRange.end)
+            }
+
+            let final = ranges.reversed()
+            print(final)
+
+            return .none
+        }
     }
 }
 
+@ViewAction(for: GoalHistoryFeature.self)
 public struct GoalHistoryView: View {
-    let store: StoreOf<GoalHistoryFeature>
+    public let store: StoreOf<GoalHistoryFeature>
 
     public init(store: StoreOf<GoalHistoryFeature>) {
         self.store = store
@@ -25,13 +87,14 @@ public struct GoalHistoryView: View {
 
     public var body: some View {
         Text("Goal History")
+            .onAppear { send(.onAppear) }
     }
 }
 
 #Preview {
     GoalHistoryView(
         store: .init(
-            initialState: .init(),
+            initialState: .init(period: .yearly),
             reducer: GoalHistoryFeature.init
         )
     )
